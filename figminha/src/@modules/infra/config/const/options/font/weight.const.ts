9c68241547d016d12413485d10d2cdfa7b7de8d2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-17 02:18:46 -0300

    [ :gear: ] | create: config - const > font [weight] (module::infra)

---
+++ figminha/src/@modules/infra/config/const/options/font/weight.const.ts
@@ -0,0 +1,16 @@
+import { IOption } from '@/@types/option.type';
+
+export const FONT_WEIGHT_OPTIONS: IOption[] = [
+  {
+    value: '400',
+    label: 'Normal',
+  },
+  {
+    value: '500',
+    label: 'Semibold',
+  },
+  {
+    value: '600',
+    label: 'Bold',
+  },
+];
