6ec4694dcd37405597e0b4d0fa2ab13ef09dd455
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-17 02:19:38 -0300

    [ :gear: ] | create: config - const > options [directions] (module::infra)

---
+++ figminha/src/@modules/infra/config/const/options/directions.const.ts
@@ -0,0 +1,6 @@
+import { IOption } from '@/@types/option.type';
+
+export const DIRECTION_OPTIONS: IOption[] = [
+  { label: 'Bring to Front', value: 'front', icon: '/assets/front.svg' },
+  { label: 'Send to Back', value: 'back', icon: '/assets/back.svg' },
+];
