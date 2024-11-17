239fcff2388a127b2cd1b36d933a45d56d5a5d19
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-17 02:21:06 -0300

    [ :gear: ] | create: config - const > shortcuts (module::infra)

---
+++ figminha/src/@modules/infra/config/const/shortcut.const.ts
@@ -0,0 +1,24 @@
+import { IShortcut } from '@/@types/shortcut.type';
+
+export const SHORTCUT: IShortcut[] = [
+  {
+    key: '1',
+    name: 'Chat',
+    shortcut: '/',
+  },
+  {
+    key: '2',
+    name: 'Undo',
+    shortcut: '⌘ + Z',
+  },
+  {
+    key: '3',
+    name: 'Redo',
+    shortcut: '⌘ + Y',
+  },
+  {
+    key: '4',
+    name: 'Reactions',
+    shortcut: 'E',
+  },
+];
