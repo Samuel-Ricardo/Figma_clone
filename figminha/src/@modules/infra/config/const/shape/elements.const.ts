9dd1d1fb6f12cdc0d69e0ce745abdc162309b547
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-17 02:20:05 -0300

    [ :gear: ] | create: config - const > elements [shape] (module::infra)

---
+++ figminha/src/@modules/infra/config/const/shape/elements.const.ts
@@ -0,0 +1,34 @@
+import { IIconElement } from '@/@types/element/icon.type';
+
+export const SHAPE_ELEMENTS: IIconElement[] = [
+  {
+    icon: '/assets/rectangle.svg',
+    name: 'Rectangle',
+    value: 'rectangle',
+  },
+  {
+    icon: '/assets/circle.svg',
+    name: 'Circle',
+    value: 'circle',
+  },
+  {
+    icon: '/assets/triangle.svg',
+    name: 'Triangle',
+    value: 'triangle',
+  },
+  {
+    icon: '/assets/line.svg',
+    name: 'Line',
+    value: 'line',
+  },
+  {
+    icon: '/assets/image.svg',
+    name: 'Image',
+    value: 'image',
+  },
+  {
+    icon: '/assets/freeform.svg',
+    name: 'Free Drawing',
+    value: 'freeform',
+  },
+];
