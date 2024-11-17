59a2561ba783fa1f613503fa2a7f5621e64ae517
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-17 02:15:22 -0300

    [ :gear: ] | create: config - const > font [family] (module::infra)

---
+++ figminha/src/@modules/infra/config/const/options/font/family.const.ts
@@ -0,0 +1,8 @@
+import { IOption } from '@/@types/option.type';
+
+export const FONT_FAMILY_OPTIONS: IOption[] = [
+  { value: 'Helvetica', label: 'Helvetica' },
+  { value: 'Times New Roman', label: 'Times New Roman' },
+  { value: 'Comic Sans MS', label: 'Comic Sans MS' },
+  { value: 'Brush Script MT', label: 'Brush Script MT' },
+];
