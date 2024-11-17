20ae0fc2c99801a64ed400c6c027251be68b49f0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-17 02:01:40 -0300

    [ :gear: ] | create: config - const (module::infra)

---
+++ figminha/src/@modules/infra/config/const/const.config.ts
@@ -0,0 +1,224 @@
+import { IIconElement } from '@/@types/element/icon.type';
+import { IOption } from '@/@types/option.type';
+import { IShortcut } from '@/@types/shortcut.type';
+
+const SHAPE_ELEMENTS: IIconElement[] = [
+  {
+    icon: '/assets/rectangle.svg',
+    name: 'Rectangle',
+    value: 'rectangle',
+  },
+  {
+    icon: '/assets/circle.svg',
+    name: 'Circle',
+    value: 'circle',
+  },
+  {
+    icon: '/assets/triangle.svg',
+    name: 'Triangle',
+    value: 'triangle',
+  },
+  {
+    icon: '/assets/line.svg',
+    name: 'Line',
+    value: 'line',
+  },
+  {
+    icon: '/assets/image.svg',
+    name: 'Image',
+    value: 'image',
+  },
+  {
+    icon: '/assets/freeform.svg',
+    name: 'Free Drawing',
+    value: 'freeform',
+  },
+];
+
+const NAV_ELEMENTS: IIconElement[] = [
+  {
+    icon: '/assets/select.svg',
+    name: 'Select',
+    value: 'select',
+  },
+  {
+    icon: '/assets/rectangle.svg',
+    name: 'Rectangle',
+    value: SHAPE_ELEMENTS,
+  },
+  {
+    icon: '/assets/text.svg',
+    value: 'text',
+    name: 'Text',
+  },
+  {
+    icon: '/assets/delete.svg',
+    value: 'delete',
+    name: 'Delete',
+  },
+  {
+    icon: '/assets/reset.svg',
+    value: 'reset',
+    name: 'Reset',
+  },
+  {
+    icon: '/assets/comments.svg',
+    value: 'comments',
+    name: 'Comments',
+  },
+];
+
+const DEFAULT_NAV_ELEMENT: IIconElement = {
+  icon: '/assets/select.svg',
+  name: 'Select',
+  value: 'select',
+};
+
+const DIRECTION_OPTIONS: IOption[] = [
+  { label: 'Bring to Front', value: 'front', icon: '/assets/front.svg' },
+  { label: 'Send to Back', value: 'back', icon: '/assets/back.svg' },
+];
+
+const FONT_FAMILY_OPTIONS: IOption[] = [
+  { value: 'Helvetica', label: 'Helvetica' },
+  { value: 'Times New Roman', label: 'Times New Roman' },
+  { value: 'Comic Sans MS', label: 'Comic Sans MS' },
+  { value: 'Brush Script MT', label: 'Brush Script MT' },
+];
+
+export const FONT_SIZE_OPTIONS: IOption[] = [
+  {
+    value: '10',
+    label: '10',
+  },
+  {
+    value: '12',
+    label: '12',
+  },
+  {
+    value: '14',
+    label: '14',
+  },
+  {
+    value: '16',
+    label: '16',
+  },
+  {
+    value: '18',
+    label: '18',
+  },
+  {
+    value: '20',
+    label: '20',
+  },
+  {
+    value: '22',
+    label: '22',
+  },
+  {
+    value: '24',
+    label: '24',
+  },
+  {
+    value: '26',
+    label: '26',
+  },
+  {
+    value: '28',
+    label: '28',
+  },
+  {
+    value: '30',
+    label: '30',
+  },
+  {
+    value: '32',
+    label: '32',
+  },
+  {
+    value: '34',
+    label: '34',
+  },
+  {
+    value: '36',
+    label: '36',
+  },
+];
+
+export const FONT_WEIGHT_OPTIONS: IOption[] = [
+  {
+    value: '400',
+    label: 'Normal',
+  },
+  {
+    value: '500',
+    label: 'Semibold',
+  },
+  {
+    value: '600',
+    label: 'Bold',
+  },
+];
+
+const ALIGN_OPTIONS: IOption[] = [
+  { value: 'left', label: 'Align Left', icon: '/assets/align-left.svg' },
+  {
+    value: 'horizontalCenter',
+    label: 'Align Horizontal Center',
+    icon: '/assets/align-horizontal-center.svg',
+  },
+  { value: 'right', label: 'Align Right', icon: '/assets/align-right.svg' },
+  { value: 'top', label: 'Align Top', icon: '/assets/align-top.svg' },
+  {
+    value: 'verticalCenter',
+    label: 'Align Vertical Center',
+    icon: '/assets/align-vertical-center.svg',
+  },
+  { value: 'bottom', label: 'Align Bottom', icon: '/assets/align-bottom.svg' },
+];
+
+const SHORTCUT: IShortcut[] = [
+  {
+    key: '1',
+    name: 'Chat',
+    shortcut: '/',
+  },
+  {
+    key: '2',
+    name: 'Undo',
+    shortcut: '⌘ + Z',
+  },
+  {
+    key: '3',
+    name: 'Redo',
+    shortcut: '⌘ + Y',
+  },
+  {
+    key: '4',
+    name: 'Reactions',
+    shortcut: 'E',
+  },
+];
+
+export const CONST = {
+  COLORS: ['#DC2626', '#D97706', '#059669', '#7C3AED', '#DB2777'],
+  SHAPE_ELEMENTS,
+  SHORTCUT,
+
+  NAV: {
+    ELEMENT: {
+      ALL: NAV_ELEMENTS,
+      DEFAULT: DEFAULT_NAV_ELEMENT,
+    },
+  },
+
+  OPTIONS: {
+    DIRECTIONS: DIRECTION_OPTIONS,
+    ALIGN: ALIGN_OPTIONS,
+    FONTS: {
+      FAMILY: FONT_FAMILY_OPTIONS,
+      SIZE: FONT_SIZE_OPTIONS,
+      WEIGHT: FONT_WEIGHT_OPTIONS,
+    },
+  },
+};
