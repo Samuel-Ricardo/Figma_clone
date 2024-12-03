f2d68fea5ecb08394c2a1726a94bfc18ae70e959
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-12-02 21:51:42 -0300

    [ :cloud: ] | update: setup - liveblocks > room (lib::liveblocks)

--- figminha/src/app/Room.tsx
+++ figminha/src/app/Room.tsx
@@ -7,13 +7,19 @@ import {
   ClientSideSuspense,
 } from '@liveblocks/react/suspense';
 import { MODULES } from '@/@modules/app.factory';
+import { LiveMap } from '@liveblocks/client';
+import Loader from './components/loader/loader.component';
 
 export function Room({ children }: { children: ReactNode }) {
   return (
     <LiveblocksProvider publicApiKey={MODULES.INFRA.CONFIG.LIVE_BLOCK.API.KEY}>
-      <RoomProvider id="my-room">
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
-          {children}
+      <RoomProvider
+        id="my-room"
+        initialPresence={{ cursor: null, cursorColor: null, editingText: null }}
+        initialStorage={{ canvasObjects: new LiveMap() }}
+      >
+        <ClientSideSuspense fallback={<Loader />}>
+          {() => children}
         </ClientSideSuspense>
       </RoomProvider>
     </LiveblocksProvider>
