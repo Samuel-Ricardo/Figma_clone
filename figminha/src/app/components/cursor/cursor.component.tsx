3bb772c7e3cddbb1972228bd40262834786919fb
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-12-04 14:59:44 -0300

    [ :art: ] | create: component - cursor (app::component)

---
+++ figminha/src/app/components/cursor/cursor.component.tsx
@@ -0,0 +1,20 @@
+import { ICursorProps } from '@/@types/props/cursor.type';
+import CursorSVG from '../../../../public/assets/CursorSVG';
+import { ChatBubble } from '../chat/bubble.component';
+
+export const Cursor = ({
+  color,
+  position: { x, y },
+  message,
+}: ICursorProps) => {
+  return (
+    <div
+      className="pointer-events-none absolute left-0 top-0"
+      style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
+    >
+      <CursorSVG color={color} />
+
+      {message && <ChatBubble color={color} message={message} />}
+    </div>
+  );
+};
