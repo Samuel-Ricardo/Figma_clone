fb95efd377d104bcbec3348daa2b4bcbf0e70467
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-12-07 12:18:16 -0300

    [ :fire: ] | clean: type - ICursorChatProps (app::component)

--- figminha/src/@types/props/chat/cursor.type.ts
+++ figminha/src/@types/props/chat/cursor.type.ts
@@ -2,11 +2,4 @@ import { IPosition } from '@/@types/position.type';
 
 export interface ICursorChatProps {
   cursor: IPosition;
-  updateMyPresence: (
-    presence: Partial<{
-      cursor: { x: number; y: number };
-      cursorColor: string;
-      message: string;
-    }>,
-  ) => void;
 }
