8e35021e553dfa8a37089fcba7e174386914e2e9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-12-07 12:22:06 -0300

    [ :adhesive_bandage: ] | fix: client/server side context (app::components)

--- figminha/src/app/components/chat/bubble.component.tsx
+++ figminha/src/app/components/chat/bubble.component.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import { IChatBubbleProps } from '@/@types/props/chat/bubble.type';
 
-export const ChatBubble = async ({ color, message }: IChatBubbleProps) => (
+export const ChatBubble = ({ color, message }: IChatBubbleProps) => (
   <div
     className="absolute left-2 top-5 rounded-3xl px-4 py-2"
     style={{ backgroundColor: color, borderRadius: 20 }}
