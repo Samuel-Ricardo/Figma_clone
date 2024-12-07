a19e1e27fd81c9c7640d71f85931fc88e77f1431
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-12-07 12:25:21 -0300

    [ :art: ] | create: component - live > reactive realtime cursors (app::component)

--- figminha/src/app/components/live/live.component.tsx
+++ figminha/src/app/components/live/live.component.tsx
@@ -1,12 +1,43 @@
+'use client';
+
 import { useOthers } from '@liveblocks/react';
 import { LiveCursors } from './cursor/cursor.component';
+import { useCursorPositionHandler } from '@/hook/cursor/state/handle/move.hook';
+import { CursorChat } from '../chat/cursor.component';
+import { useCursorStateHandleByKey } from '@/hook/cursor/state/handle/by/key.hook';
+import { useEffect } from 'react';
 
 export const Live = () => {
   const others = useOthers();
+  const { setupWindowKeyListeners, removeWindowKeyListeners } =
+    useCursorStateHandleByKey({ message: null });
+
+  const {
+    cursor,
+    onPointerDown,
+    onPointerMove,
+    onPointerUp,
+    onPointerLeave,
+    onPointerEnter,
+  } = useCursorPositionHandler();
+
+  useEffect(() => {
+    setupWindowKeyListeners();
+    return () => removeWindowKeyListeners();
+  }, [setupWindowKeyListeners, removeWindowKeyListeners]);
 
   return (
-    <div className="h-[100vh] w-full flex justify-center items-center text-center border-5 border-green-500">
+    <div
+      className="h-[100vh] w-full flex justify-center items-center text-center border-5 border-green-500"
+      onPointerEnter={onPointerEnter}
+      onPointerDown={onPointerDown}
+      onPointerMove={onPointerMove}
+      onPointerUp={onPointerUp}
+      onPointerLeave={onPointerLeave}
+    >
       <h1 className="text-2xl text-white">Hello World</h1>
+
+      {cursor && <CursorChat cursor={cursor} />}
 
       <LiveCursors others={others} />
     </div>
