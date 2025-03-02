eb7cae924616ef54d806a5a859c5d314b335e093
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-02 00:32:14 -0300

    [ :art: ] | update: component - live > implements real time reactions (app::component)

--- figminha/src/app/components/live/live.component.tsx
+++ figminha/src/app/components/live/live.component.tsx
@@ -2,16 +2,71 @@
 
 import { useOthers } from '@liveblocks/react';
 import { LiveCursors } from './cursor/cursor.component';
-import { useCursorPositionHandler } from '@/hook/cursor/state/handle/move.hook';
 import { CursorChat } from '../chat/cursor.component';
 import { useCursorStateHandleByKey } from '@/hook/cursor/state/handle/by/key.hook';
 import { useEffect } from 'react';
+import { useCursorPointerHandler } from '@/hook/cursor/pointer/handler.hook';
+import { useMyPresenceHandler } from '@/hook/presence.hook';
+import { FloatingReaction } from '../reaction/floating/floating.component';
+import { useReactionState } from '@/store/reaction/reaction.store';
+import { useCursorState } from '@/store/cursor/cursor.store';
+import { REACTION, REACTION_SELECTOR } from '@/@types/props/cursor/mode.type';
+import { ReactionSelector } from '../reaction/selector/selector.component';
+import { Cursor } from '../cursor/cursor.component';
+import { useReactionStartup } from '@/hook/reaction/startup.hook';
 
 export const Live = () => {
   const others = useOthers();
   const { setupWindowKeyListeners, removeWindowKeyListeners } =
     useCursorStateHandleByKey({ message: null });
 
+  const { reactions } = useReactionState();
+  const { state } = useCursorState();
+
+  const { handlePointerLeave, handlePointerMove, handlePointerEnter } =
+    useCursorPointerHandler();
+
+  const { cursor } = useMyPresenceHandler();
+
+  useEffect(() => {
+    setupWindowKeyListeners();
+    return () => removeWindowKeyListeners();
+  }, [setupWindowKeyListeners, removeWindowKeyListeners]);
+
+  useReactionStartup();
+  return (
+    <div
+      className="bg-transparent h-[100vh] w-full flex justify-center items-center text-center border-5 border-green-500"
+      onPointerEnter={handlePointerEnter}
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+    >
+      <h1 className="text-2xl text-black italic">Hello World</h1>
+
+      {cursor && (
+        <Cursor color="black" position={{ x: cursor.x, y: cursor.y }} />
+      )}
+
+      {reactions.map(reaction => (
+        <FloatingReaction
+          key={reaction.timestamp}
+          reaction={reaction.reaction}
+          timestamp={reaction.timestamp}
+          positition={reaction.position}
+        />
+      ))}
+
+      {cursor && <CursorChat cursor={cursor} />}
+
+      {(state.mode === REACTION_SELECTOR || state.mode === REACTION) &&
+        cursor && <ReactionSelector />}
+
+      <LiveCursors others={others} />
+    </div>
+  );
+};
+
+/*
   const {
     cursor,
     onPointerDown,
@@ -20,26 +75,4 @@ export const Live = () => {
     onPointerLeave,
     onPointerEnter,
   } = useCursorPositionHandler();
-
-  useEffect(() => {
-    setupWindowKeyListeners();
-    return () => removeWindowKeyListeners();
-  }, [setupWindowKeyListeners, removeWindowKeyListeners]);
-
-  return (
-    <div
-      className="h-[100vh] w-full flex justify-center items-center text-center border-5 border-green-500"
-      onPointerEnter={onPointerEnter}
-      onPointerDown={onPointerDown}
-      onPointerMove={onPointerMove}
-      onPointerUp={onPointerUp}
-      onPointerLeave={onPointerLeave}
-    >
-      <h1 className="text-2xl text-white">Hello World</h1>
-
-      {cursor && <CursorChat cursor={cursor} />}
-
-      <LiveCursors others={others} />
-    </div>
-  );
-};
+  */
