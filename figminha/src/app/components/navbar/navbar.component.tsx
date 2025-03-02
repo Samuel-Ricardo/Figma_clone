a701e2267992f44f99004494d2b562370af4f225
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-02 18:12:23 -0300

    [ :art: ] | create: component - navbar (app::ui)

---
+++ figminha/src/app/components/navbar/navbar.component.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import Logo from '../../../../public/assets/logo.svg';
+
+export const Navbar = ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <nav className="navbar--container">
+      <Image src={Logo} alt="logo" width={58} height={20} />
+      {children}
+    </nav>
+  );
+};
