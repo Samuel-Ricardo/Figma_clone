7465a09a0d9a64586ad9878d6492b51620d456fa
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-02 18:13:04 -0300

    [ :art: ] | setup: component - navbar (app::ui)

--- figminha/src/app/page.tsx
+++ figminha/src/app/page.tsx
@@ -1,9 +1,16 @@
 import './globals.scss';
 import { Live } from './components/live/live.component';
+import { Navbar } from './components/navbar/navbar.component';
+import { NavbarToolbar } from './components/navbar/toolbar/toolbar.component';
+import { ActiveUsers } from './components/user/active/active.component';
 
 export default function Page() {
   return (
     <div className="app-root-container">
+      <Navbar>
+        <NavbarToolbar />
+        <ActiveUsers />
+      </Navbar>
       <Live />
     </div>
   );
