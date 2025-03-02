30fea7dc86ef10237fef67e1e5a09ab80f282463
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-02 00:07:53 -0300

    [ :link: ] | update: store - reaction (app::store)

--- figminha/src/@types/store/reaction.type.ts
+++ figminha/src/@types/store/reaction.type.ts
@@ -1,7 +1,22 @@
+import { IPosition } from '../position.type';
 import { Reaction } from '../props/reaction/reaction.type';
 
 export interface IReactionStore {
   reaction: Reaction | null;
+  reactions: IReactionAnimation[];
   setReaction: (reaction: Reaction) => void;
-  clearReaction: () => void;
+  clearReactions: () => void;
+  addReaction: ({ position, reaction }: IReactionEvent) => void;
+}
+
+export interface IReactionAnimation {
+  reaction: Reaction;
+  timestamp: number;
+  position: IPosition;
+}
+
+export interface IReactionEvent {
+  position: IPosition;
+  reaction: Reaction;
+  timestamp?: number;
 }
