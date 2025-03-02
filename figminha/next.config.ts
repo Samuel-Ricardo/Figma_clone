9955b57538d997745cf46c3aff9c5f8928dc89ab
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-02 17:43:31 -0300

    [ :gear: ] | setup: next - image > livebocks.io (app::config)

--- figminha/next.config.ts
+++ figminha/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['liveblocks.io'],
+  },
 };
 
 export default nextConfig;
