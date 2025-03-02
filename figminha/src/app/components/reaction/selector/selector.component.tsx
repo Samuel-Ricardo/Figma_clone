364d3452b55c37dc301472a049c8f56a33e38860
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-02 00:30:32 -0300

    [ :art: ] | update: component - reaction > selector [mouse click] (app::style)

--- figminha/src/app/components/reaction/selector/selector.component.tsx
+++ figminha/src/app/components/reaction/selector/selector.component.tsx
@@ -1,16 +1,27 @@
+'use client';
 import './selector.style.scss';
 
 import { IReactionSelectorProps } from '@/@types/props/reaction/selector/reaction.type';
 import { ReactionSelectorButton } from './button/button.component';
 import { MODULES } from '@/@modules/app.factory';
+import { useMyPresenceHandler } from '@/hook/presence.hook';
+import { useCursorPointerHandler } from '@/hook/cursor/pointer/handler.hook';
 
 export const ReactionSelector = ({ onSelect }: IReactionSelectorProps) => {
   const { REACTIONS } = MODULES.INFRA.CONFIG.CONST();
+  const { cursor } = useMyPresenceHandler();
+
+  const { handlePointerDown, handlePointerUp } = useCursorPointerHandler();
 
   return (
     <ul
       className="reaction-selector--container"
+      style={{
+        transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
+      }}
       onPointerMove={e => e.stopPropagation()}
+      onPointerDown={handlePointerDown}
+      onPointerUp={handlePointerUp}
     >
       {REACTIONS.map(reaction => (
         <ReactionSelectorButton
