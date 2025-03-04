0d79efe90ebfbaa36c7e6f5e0732286f46f02b0c
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-03 22:38:13 -0300

    [ :art: ] | create: component - sidebar > right (app::ui)

---
+++ figminha/src/app/components/sidebar/right/right.component.tsx
@@ -0,0 +1,8 @@
+import './right.style.scss';
+
+export const RightSidebar = () => (
+  <section id="right-sidebar--container">
+    <h3>Design</h3>
+    <span>Make Changes to Canvas as You Like</span>
+  </section>
+);
