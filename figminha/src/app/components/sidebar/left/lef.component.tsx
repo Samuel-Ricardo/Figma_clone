9cb9222c38740bd6b71c54db775d494ad880489b
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-03 22:39:37 -0300

    [ :art: ] | create: component - sidebar > left (app::ui)

---
+++ figminha/src/app/components/sidebar/left/lef.component.tsx
@@ -0,0 +1,10 @@
+import './left.style.scss';
+
+export const LeftSidebar = ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => (
+  <section id="left-sidebar--container">
+    <h3>Layers</h3>
+    <div className="left-sidebar--layers-container">{children}</div>
+  </section>
+);
