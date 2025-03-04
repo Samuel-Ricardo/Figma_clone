de66b39c827531454c9a25e47e3fe63479186550
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-03 22:41:54 -0300

    [ :page_with_curl: ] | update: page - home > sidebars (app::page)

--- figminha/src/app/page.tsx
+++ figminha/src/app/page.tsx
@@ -3,6 +3,9 @@ import { Live } from './components/live/live.component';
 import { Navbar } from './components/navbar/navbar.component';
 import { NavbarToolbar } from './components/navbar/toolbar/toolbar.component';
 import { ActiveUsers } from './components/user/active/active.component';
+import { LeftSidebar } from './components/sidebar/left/lef.component';
+import { RightSidebar } from './components/sidebar/right/right.component';
+import { ElementLayers } from './components/sidebar/left/layers/layers.component';
 
 export default function Page() {
   return (
@@ -11,7 +14,13 @@ export default function Page() {
         <NavbarToolbar />
         <ActiveUsers />
       </Navbar>
-      <Live />
+      <section id="main-section--container">
+        <LeftSidebar>
+          <ElementLayers />
+        </LeftSidebar>
+        <Live />
+        <RightSidebar />
+      </section>
     </div>
   );
 }
