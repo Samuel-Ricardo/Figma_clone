edf18f203abf786b3b0c9759daeedd535acbd5fb
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-16 17:14:33 -0300

    [ :iphone: ] | fix: responsivity (app::style)

--- figminha/src/app/components/sidebar/left/lef.component.tsx
+++ figminha/src/app/components/sidebar/left/lef.component.tsx
@@ -3,7 +3,7 @@ import './left.style.scss';
 export const LeftSidebar = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => (
-  <section id="left-sidebar--container">
+  <section id="left-sidebar--container" className="max-sm:hidden">
     <h3>Layers</h3>
     <div className="left-sidebar--layers-container">{children}</div>
   </section>
