15ec46b7c5c5f8b7caecc3f1b0e7113a8d7a2a51
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-22 11:21:43 -0300

    [ :label: ] | update: type - store > canvas (app::store)

--- figminha/src/@types/store/canvas/canvas.type.ts
+++ figminha/src/@types/store/canvas/canvas.type.ts
@@ -1,6 +1,32 @@
+import { LiveMap } from '@liveblocks/client';
+import { Object } from 'fabric';
+
 export interface ICanvasStore {
   canvasRef?: React.MutableRefObject<HTMLCanvasElement | null> | null;
+  isDrawing: boolean;
+  isEditing: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  canvasObjects?: LiveMap<string, any> | null;
+  activeObjectRef?: React.MutableRefObject<Object | null> | null;
+  imageInputRef?: React.MutableRefObject<HTMLInputElement | null> | null;
+
   setCanvasRef: (
     canvasRef: React.MutableRefObject<HTMLCanvasElement | null> | null,
   ) => void;
+  setActiveObjectRef: (
+    activeObjectRef: React.MutableRefObject<Object | null> | null,
+  ) => void;
+  setImageInputRef: (
+    imageInputRef: React.MutableRefObject<HTMLInputElement | null> | null,
+  ) => void;
+
+  startDrawing: () => void;
+  stopDrawing: () => void;
+
+  startEditing: () => void;
+  stopEditing: () => void;
+
+  clearCanvasRef: () => void;
+  clearActiveObjectRef: () => void;
+  clearImageInputRef: () => void;
 }
