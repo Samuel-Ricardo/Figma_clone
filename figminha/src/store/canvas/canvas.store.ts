c7be927cbee60b8474eaca511f91ab9d5e8481cc
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-22 11:41:10 -0300

    [ :link: ] | update: store - canvas (app::store)

--- figminha/src/store/canvas/canvas.store.ts
+++ figminha/src/store/canvas/canvas.store.ts
@@ -3,5 +3,23 @@ import { create } from 'zustand';
 
 export const useCanvasStore = create<ICanvasStore>(set => ({
   canvasRef: null,
+  isDrawing: false,
+  isEditing: false,
+  canvasObjects: null, //useStorage(root => root.canvasObjects)
+  activeObjectRef: null,
+  imageInputRef: null,
+
   setCanvasRef: canvasRef => set({ canvasRef }),
+  setActiveObjectRef: activeObjectRef => set({ activeObjectRef }),
+  setImageInputRef: imageInputRef => set({ imageInputRef }),
+
+  startDrawing: () => set({ isDrawing: true }),
+  stopDrawing: () => set({ isDrawing: false }),
+
+  startEditing: () => set({ isEditing: true }),
+  stopEditing: () => set({ isEditing: false }),
+
+  clearActiveObjectRef: () => set({ activeObjectRef: null }),
+  clearCanvasRef: () => set({ canvasRef: null }),
+  clearImageInputRef: () => set({ imageInputRef: null }),
 }));
