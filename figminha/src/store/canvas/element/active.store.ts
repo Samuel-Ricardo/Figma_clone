422a07c29dd406ea59bc8f0d2d9576eca9e4b2a2
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-22 11:47:51 -0300

    [ :link: ] | create: store - canvas > element (app::store)

--- figminha/src/store/canvas/element/active.store.ts
+++ figminha/src/store/canvas/element/active.store.ts
@@ -3,15 +3,21 @@ import { create } from 'zustand';
 
 export const useActiveElement = create<IActiveElementStore>(set => ({
   element: null,
-  imageInputRef: null,
+  attributes: {
+    width: '0px',
+    height: '0px',
+    fontFamily: 'Arial',
+    fontSize: '0px',
+    fontWeight: '0',
+    fill: '#aabbcc',
+    stroke: '#aabbcc',
+  },
 
   isActive: (value, element) =>
     (element && element.value === value) ||
     (Array.isArray(value) && value.some(val => val?.value === element?.value)),
 
   setElement: element => set({ element }),
-  setImageInputRef: imageInputRef => set({ imageInputRef }),
 
   clearElement: () => set({ element: null }),
-  clearImageInputRef: () => set({ imageInputRef: null }),
 }));
