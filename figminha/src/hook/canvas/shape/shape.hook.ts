0c0f1222319356c575aafdabad8f44aa3d85963e
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-22 11:55:30 -0300

    [ :link: ] | create: hook - canvas > shape [delete::all] (app::canvas)

--- figminha/src/hook/canvas/shape/shape.hook.ts
+++ figminha/src/hook/canvas/shape/shape.hook.ts
@@ -6,6 +6,17 @@ export const useShape = () => {
     canvasObjects.delete(shapeId);
   }, []);
 
+  const deleteAllShapes = useMutation(({ storage }) => {
+    const canvasObjects = storage.get('canvasObjects');
+    if (!canvasObjects || canvasObjects.size === 0) return true;
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    for (const [key, _value] of canvasObjects.entries())
+      canvasObjects.delete(key);
+
+    return canvasObjects.size === 0;
+  }, []);
+
   return {
     delete: {
       shape: deleteShapeFromStorage,
