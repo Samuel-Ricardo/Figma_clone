d6158e9ef4c574d70475aa3879c9f3858895e843
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-22 11:31:10 -0300

    [ :iphone: ] | fix: responsivity - sidebars (app::style)

--- figminha/src/app/components/sidebar/right/right.component.tsx
+++ figminha/src/app/components/sidebar/right/right.component.tsx
@@ -1,7 +1,7 @@
 import './right.style.scss';
 
 export const RightSidebar = () => (
-  <section id="right-sidebar--container">
+  <section id="right-sidebar--container" className="max-sm:hidden">
     <h3>Design</h3>
     <span>Make Changes to Canvas as You Like</span>
   </section>
