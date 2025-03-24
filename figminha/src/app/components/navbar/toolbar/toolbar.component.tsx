2823b0607a49068989a4586eebe311bc7ff1ce71
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-24 16:09:22 -0300

    [ :recycle: ] | refact: rename - store > useActiveElement -> useActiveElementStore (app::hook)

--- figminha/src/app/components/navbar/toolbar/toolbar.component.tsx
+++ figminha/src/app/components/navbar/toolbar/toolbar.component.tsx
@@ -4,14 +4,14 @@ import './toolbar.style.scss';
 
 import { MODULES } from '@/@modules/app.factory';
 import { IElement } from '@/@types/store/canvas/element/active.type';
-import { useActiveElement } from '@/store/canvas/element/active.store';
+import { useActiveElementStore } from '@/store/canvas/element/active.store';
 import { useCallback } from 'react';
 
 export const NavbarToolbar = () => {
   //const {} = useActiveElementHandler();
   //const {} = useImageUploadHandler();
   const handleActiveElement = (value: string) => console.log({ value });
-  const { isActive, element } = useActiveElement();
+  const { isActive, element } = useActiveElementStore();
 
   const onClick = useCallback((item: IElement) => {
     if (!Array.isArray(item.value)) handleActiveElement(item.value);
