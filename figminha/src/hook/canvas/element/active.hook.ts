0550a9251c3476fd58b5ce6b30a82e0ce9b70575
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-24 16:22:34 -0300

    [ :link: ] | create: hook - canvas > active element [resetDefault] (app::canvas)

--- figminha/src/hook/canvas/element/active.hook.ts
+++ figminha/src/hook/canvas/element/active.hook.ts
@@ -1,7 +1,24 @@
+/* eslint-disable @typescript-eslint */
+
+import { useActiveElementStore } from '@/store/canvas/element/active.store';
+import { useCallback } from 'react';
+import { useShape } from '../shape/shape.hook';
+import { useCanvas } from '../canvas.hook';
+import { useCanvasStore } from '@/store/canvas/canvas.store';
+import { useShapeStore } from '@/store/canvas/shape/shape.store';
+
 export const useActiveElement = () => {
-  //  const { clearElement, resetDefault, setElement } = useActiveElementStore();
-  //  const { deleteAllShapes } = useShape();
-  //  const { setSelectedShapeRef } = useShapeStore();
-  //  const { deleteElementHandler, stopDrawn } = useCanvas();
-  //  const { imageInputRef } = useCanvasStore();
+  const { clearElement, resetDefault, setElement } = useActiveElementStore();
+  const { deleteAllShapes } = useShape();
+  const { setSelectedShapeRef } = useShapeStore();
+  const { deleteElementHandler, stopDrawn } = useCanvas();
+  const { imageInputRef } = useCanvasStore();
+
+  const resetElement = useCallback(() => {
+    deleteAllShapes();
+    clearElement();
+    resetDefault();
+  }, [deleteAllShapes, clearElement, resetDefault]);
+
+  return {};
 };
