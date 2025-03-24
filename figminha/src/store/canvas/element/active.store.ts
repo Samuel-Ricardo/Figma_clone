527962ce408e1b5a037153dd99f47338950d9369
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-24 16:08:27 -0300

    [ :link: ] | update: store - canvas > element [default values] (app::store)

--- figminha/src/store/canvas/element/active.store.ts
+++ figminha/src/store/canvas/element/active.store.ts
@@ -1,23 +1,35 @@
 import { IActiveElementStore } from '@/@types/store/canvas/element/active.type';
 import { create } from 'zustand';
 
-export const useActiveElement = create<IActiveElementStore>(set => ({
-  element: null,
-  attributes: {
-    width: '0px',
-    height: '0px',
-    fontFamily: 'Arial',
-    fontSize: '0px',
-    fontWeight: '0',
-    fill: '#aabbcc',
-    stroke: '#aabbcc',
-  },
+const defaultAtributes = {
+  width: '0px',
+  height: '0px',
+  fontFamily: 'Arial',
+  fontSize: '0px',
+  fontWeight: '0',
+  fill: '#aabbcc',
+  stroke: '#aabbcc',
+};
+
+const defaultElement = {
+  icon: '/assets/select.svg',
+  name: 'Select',
+  value: 'select',
+};
+
+export const useActiveElementStore = create<IActiveElementStore>(set => ({
+  element: defaultElement,
+  attributes: defaultAtributes,
 
   isActive: (value, element) =>
     (element && element.value === value) ||
     (Array.isArray(value) && value.some(val => val?.value === element?.value)),
 
   setElement: element => set({ element }),
-
+  resetDefault: () =>
+    set({
+      element: defaultElement,
+      attributes: defaultAtributes,
+    }),
   clearElement: () => set({ element: null }),
 }));
