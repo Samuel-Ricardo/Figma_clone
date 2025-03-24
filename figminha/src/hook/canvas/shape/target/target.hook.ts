2222c9de891115826e64d95d6cbf957b55ce1437
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-24 16:25:39 -0300

    [ :link: ] | create: hook - canvas > shape > target (app::canvas)

---
+++ figminha/src/hook/canvas/shape/target/target.hook.ts
@@ -0,0 +1,11 @@
+import { useFabricState } from '@/store/canvas/fabric/fabric.store';
+import { useShapeStore } from '@/store/canvas/shape/shape.store';
+import { FabricObject } from 'fabric';
+import { useCallback } from 'react';
+import { useCanvas } from '../../canvas.hook';
+
+export const useShapeTarget = () => {
+  const { stopDrawn } = useCanvas();
+  const { selectedShapeRef } = useShapeStore();
+  const { fabricRef } = useFabricState();
+};
