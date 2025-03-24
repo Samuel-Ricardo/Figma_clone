647afc3a83ce2d9c8e1447332a3befc13c13b7e8
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-24 16:07:33 -0300

    [ :label: ] | update: type - store > shape [selectedShapeRef] (app::store)

--- figminha/src/@types/store/canvas/shape/shape.type.ts
+++ figminha/src/@types/store/canvas/shape/shape.type.ts
@@ -1,15 +1,14 @@
+import { ShapeType } from '@/@types/canvas/shape/type.type';
 import { Object } from 'fabric';
 
 export interface IShapeStore {
   shapeRef: React.MutableRefObject<Object | null> | null;
-  selectedShapeRef: React.MutableRefObject<Object | null> | null;
+  selectedShapeRef: ShapeType | null;
 
   setShapeRef: (shape: React.MutableRefObject<Object | null> | null) => void;
   clearShapeRef: () => void;
 
-  setSelectedShapeRef: (
-    shape: React.MutableRefObject<Object | null> | null,
-  ) => void;
+  setSelectedShapeRef: (shape: string | null) => void;
   clearSelectedShapeRef: () => void;
 
   clearAllShapeRefs: () => void;
