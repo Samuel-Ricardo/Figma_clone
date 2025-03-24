f9c6b4fb4337554c0d1a3d99f74b0c32d081e379
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-24 16:17:17 -0300

    [ :link: ] | create: hook - canvas > active element (app::canvas)

---
+++ figminha/src/hook/canvas/element/active.hook.ts
@@ -0,0 +1,7 @@
+export const useActiveElement = () => {
+  //  const { clearElement, resetDefault, setElement } = useActiveElementStore();
+  //  const { deleteAllShapes } = useShape();
+  //  const { setSelectedShapeRef } = useShapeStore();
+  //  const { deleteElementHandler, stopDrawn } = useCanvas();
+  //  const { imageInputRef } = useCanvasStore();
+};
