3bb50c367c00ed2828b973c0a35358cea82fe1a8
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-28 17:24:31 -0300

    [ :link: ] | create: canvas - clamp > target [position] (app::hook)

--- figminha/src/hook/canvas/actions/actions.hook.ts
+++ figminha/src/hook/canvas/actions/actions.hook.ts
@@ -1,11 +1,28 @@
+import { IClampTargetPosition } from '@/@types/canvas/target/position/clamp/clamp.type';
 import { useRedo, useUndo } from '@liveblocks/react';
+import { useCallback } from 'react';
 
 export const useCanvasActions = () => {
   const undo = useUndo();
   const redo = useRedo();
 
+  const clampTargetPositionX = useCallback(
+    ({ canvas, target }: IClampTargetPosition) => {
+      target.left = Math.max(
+        0,
+        Math.min(
+          target.left,
+          (canvas?.width || 0) - (target.getScaledWidth() || target.width || 0),
+        ),
+      );
+    },
+    [],
+  );
+
   return {
     undo,
     redo,
+    clampTargetPositionX,
+    //    clampTargetPositionY,
   };
 };
