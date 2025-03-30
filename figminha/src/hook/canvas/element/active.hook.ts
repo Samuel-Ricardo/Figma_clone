de7027bb3ad0706372e476d9aa66f74d38f5d224
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-03-29 23:19:20 -0300

    [ :link: ] | hook: active element - sync > attributes (app::canvas)

--- figminha/src/hook/canvas/element/active.hook.ts
+++ figminha/src/hook/canvas/element/active.hook.ts
@@ -5,9 +5,11 @@ import { useCanvas } from '../canvas.hook';
 import { useCanvasStore } from '@/store/canvas/canvas.store';
 import { IElement } from '@/@types/store/canvas/element/active.type';
 import { useShapeStore } from '@/store/canvas/shape/shape.store';
+import { ISyncCanvasElementAttributes } from '@/@types/canvas/element/attribute/sync.type';
 
 export const useActiveElement = () => {
-  const { clearElement, resetDefault, setElement } = useActiveElementStore();
+  const { clearElement, resetDefault, setElement, setElementAttributes } =
+    useActiveElementStore();
   const { deleteAllShapes } = useShape();
   const { setSelectedShapeRef } = useShapeStore();
   const { deleteElementHandler, stopDrawn } = useCanvas();
@@ -63,5 +65,31 @@ export const useActiveElement = () => {
     ],
   );
 
-  return { handleActiveElement };
+  const syncElementAttributes = useCallback(
+    ({ selected }: ISyncCanvasElementAttributes) => {
+      const scaledWidth = selected.scaleX
+        ? selected.width * selected.scaleX
+        : selected.width;
+
+      const scaledHeight = selected.scaleY
+        ? selected.height * selected.scaleY
+        : selected.height;
+
+      setElementAttributes({
+        width: scaledWidth.toFixed(0).toString() || '',
+        height: scaledHeight.toFixed(0).toString() || '',
+        fill: selected.fill?.toString() || '',
+        stroke: selected.stroke?.toString() || '',
+        //@ts-expect-error ts-migrate(2339) FIXME: Property 'fontSize' does not exist on type 'ICustomFabricObject'...
+        fontSize: selected.fontSize || '',
+        //@ts-expect-error ts-migrate(2339) FIXME: Property 'fontFamily' does not exist on type 'ICustomFabricObject'...
+        fontFamily: selected.fontFamily || '',
+        //@ts-expect-error ts-migrate(2339) FIXME: Property 'fontWeight' does not exist on type 'ICustomFabricObject'...
+        fontWeight: selected.fontWeight || '',
+      });
+    },
+    [setElementAttributes],
+  );
+
+  return { handleActiveElement, syncElementAttributes };
 };
