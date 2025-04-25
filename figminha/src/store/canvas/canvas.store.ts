b67f41e1b1c676842fc70bda88aa87c3c422e9ac
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-25 00:06:32 -0300

    [ :link: ] | update: store - canvas > handle clipboard (app::store)

--- figminha/src/store/canvas/canvas.store.ts
+++ figminha/src/store/canvas/canvas.store.ts
@@ -3,6 +3,7 @@ import { create } from 'zustand';
 
 export const useCanvasStore = create<ICanvasStore>(set => ({
   canvasRef: null,
+  clipboard: [],
   isDrawing: false,
   isEditing: false,
   canvasObjects: null, //useStorage(root => root.canvasObjects)
@@ -12,6 +13,21 @@ export const useCanvasStore = create<ICanvasStore>(set => ({
   setCanvasRef: canvasRef => set({ canvasRef }),
   setActiveObjectRef: activeObjectRef => set({ activeObjectRef }),
   setImageInputRef: imageInputRef => set({ imageInputRef }),
+  setClipboard: clipboard => {
+    localStorage.setItem('clipboard', JSON.stringify(clipboard));
+    set({ clipboard });
+  },
+
+  clearClipboard: () => {
+    localStorage.removeItem('clipboard');
+    set({ clipboard: [] });
+  },
+
+  getClipboard: () => {
+    const clipboard = JSON.parse(localStorage.getItem('clipboard') || '[]');
+    set({ clipboard });
+    return clipboard;
+  },
 
   startDrawing: () => set({ isDrawing: true }),
   stopDrawing: () => set({ isDrawing: false }),
