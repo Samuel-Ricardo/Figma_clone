3319f42242e45d4243e51d97ccb2069a61db4373
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-28 21:59:59 -0300

    [ :link: ] | update: hook - canvas > paste from clipboard (app::hook)

--- figminha/src/hook/canvas/canvas.hook.ts
+++ figminha/src/hook/canvas/canvas.hook.ts
@@ -3,13 +3,14 @@ import { useShape } from './shape/shape.hook';
 import { useFabricState } from '@/store/canvas/fabric/fabric.store';
 import { ICustomFabricObject } from '@/@types/fabric/object.type';
 import { useCanvasStore } from '@/store/canvas/canvas.store';
-import { Canvas, Point } from 'fabric';
+import { Canvas, Object, Point, util } from 'fabric';
 import { ICanvasDynamicZoom } from '@/@types/canvas/zoom.type';
+import { v4 as uuidv4 } from 'uuid';
 
 export const useCanvas = () => {
-  const { deleteShapeFromStorage } = useShape();
+  const { syncShapeInStorage, deleteShapeFromStorage } = useShape();
   const { fabricRef } = useFabricState();
-  const { canvasRef } = useCanvasStore();
+  const { canvasRef, setClipboard, getClipboard } = useCanvasStore();
   const { stopDrawing, startDrawing } = useCanvasStore();
 
   const deleteElementHandler = useCallback(() => {
@@ -88,10 +89,50 @@ export const useCanvas = () => {
     if (activeObjects.length <= 0) return activeObjects;
 
     const serializedObjects = activeObjects?.map(obj => obj.toObject());
-    localStorage.setItem('clipboard', JSON.stringify(serializedObjects));
+    setClipboard(serializedObjects);
 
     return activeObjects;
-  }, [fabricRef]);
+  }, [fabricRef, setClipboard]);
+
+  const offsetToAvoidOverlaps = useCallback(
+    (enlivenedObject: Object) => {
+      enlivenedObject.set({
+        left: enlivenedObject.left || 0 + 20,
+        top: enlivenedObject.top || 0 + 20,
+        objectId: uuidv4(),
+        fill: '#aabbcc',
+      } as ICustomFabricObject);
+
+      fabricRef?.current?.add(enlivenedObject);
+      syncShapeInStorage(enlivenedObject);
+    },
+    [fabricRef, syncShapeInStorage],
+  );
+
+  const deserialize = useCallback(
+    (obj: Object) => {
+      util.enlivenObjects([obj], {
+        signal: new AbortController().signal,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        reviver: (enlivenedObjects, _instance) => {
+          enlivenedObjects.forEach(offsetToAvoidOverlaps);
+          fabricRef?.current?.renderAll();
+        },
+      });
+    },
+    [fabricRef, offsetToAvoidOverlaps],
+  );
+
+  const paste = useCallback(() => {
+    if (!fabricRef?.current)
+      return console.error('Invalid Canvas Object. Aborting Paste Operation.');
+
+    const clipboard = getClipboard();
+    if (!clipboard)
+      return console.log('No Clipboard. Aborting Paste Operation.');
+
+    clipboard.forEach(deserialize);
+  }, [deserialize, fabricRef, getClipboard]);
 
   return {
     deleteElementHandler,
@@ -101,5 +142,6 @@ export const useCanvas = () => {
     dynamicZoom,
     resize,
     copy,
+    paste,
   };
 };
