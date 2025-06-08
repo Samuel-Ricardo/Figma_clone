d369f59e2c443ff04a588b1865bdeddd7bc5a9a5
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-06-07 23:41:37 -0300

    [ :art: ] | setup: canvas - render (hook::canvas)

--- figminha/src/hook/canvas/setup.hook.ts
+++ figminha/src/hook/canvas/setup.hook.ts
@@ -1,5 +1,13 @@
+import { useEffect } from 'react';
 import { useCanvasGestures } from './gestures/gestures.hook';
+import { useCanvasStore } from '@/store/canvas/canvas.store';
+import { useCanvas } from './canvas.hook';
 
 export const useSetupCanvas = () => {
+  const { canvasObjects } = useCanvasStore();
+  const { render } = useCanvas();
+
   useCanvasGestures();
+
+  useEffect(() => render(), [canvasObjects]);
 };
