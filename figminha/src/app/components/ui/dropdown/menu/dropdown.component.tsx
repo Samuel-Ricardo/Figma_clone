d58c2c9ac30fc0f6de3c220301d6e4db5e63c6ad
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-07-01 22:02:16 -0300

    [ :art: ] | create: component - dropdown (app::ui)

---
+++ figminha/src/app/components/ui/dropdown/menu/dropdown.component.tsx
@@ -0,0 +1,15 @@
+import {
+  Root,
+  Trigger,
+  Group,
+  Portal,
+  Sub,
+  RadioGroup,
+} from '@radix-ui/react-dropdown-menu';
+
+export const DropdownMenu = Root;
+export const DropdownMenuTrigger = Trigger;
+export const DropdownMenuGroup = Group;
+export const DropdownMenuSub = Sub;
+export const DropdownMenuPortal = Portal;
+export const DropdownMenuRadioGroup = RadioGroup;
