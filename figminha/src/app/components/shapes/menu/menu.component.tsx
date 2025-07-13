04da1436915eff5ad3fb19d366a82c916b102661
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-07-13 02:22:46 -0300

    [ :art: ] | create: component - menu > shapes (app::ui)

---
+++ figminha/src/app/components/shapes/menu/menu.component.tsx
@@ -0,0 +1,32 @@
+import {
+  ActiveElement,
+  IElement,
+} from '@/@types/store/canvas/element/active.type';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+} from '@radix-ui/react-dropdown-menu';
+import { Button } from '../../ui/button/button.component';
+import { ShapesMenuIcon } from './icon/icon.component';
+import { ShapesMenuItemOptions } from './options/options.component';
+
+export interface IShapeMenuProps {
+  item: IElement<Array<ActiveElement>>;
+}
+
+export const ShapesMenu = ({ item }: IShapeMenuProps) => {
+  console.log({ item });
+  return (
+    <>
+      <DropdownMenu>
+        <ShapesMenuIcon item={item} />
+
+        <DropdownMenuContent className="shapes-menu--content">
+          {item.value.map(element => (
+            <ShapesMenuItemOptions item={element} key={element?.value} />
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
