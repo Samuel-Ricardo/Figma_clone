f6236ae41270efc8b6926733ea70c5be46b3a16f
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-07-26 11:11:12 -0300

    [ :label: ] | update: enum - comment > state [creation] (@type::comments)

--- figminha/src/@types/comment/state/creation.type.ts
+++ figminha/src/@types/comment/state/creation.type.ts
@@ -1,5 +1,9 @@
-export enum CreatingCommentState {
+export enum CreationCommentState {
   PLACING = 'PLACING',
   PLACED = 'PLACED',
   COMPLETED = 'COMPLETED',
 }
+
+export const PLACING = CreationCommentState.PLACING;
+export const PLACED = CreationCommentState.PLACED;
+export const COMPLETED = CreationCommentState.COMPLETED;
