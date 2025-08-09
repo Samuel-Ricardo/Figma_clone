7e5205367d30cebf68dfc594fa27eb1b2cb6ce1c
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-08-09 17:01:31 -0300

    [ :link: ] | update: hook - mouse > capture [last pointer action] (app::store)

--- figminha/src/hook/mouse/click/composer.hook.ts
+++ figminha/src/hook/mouse/click/composer.hook.ts
@@ -1,4 +1,8 @@
+import { usePointerStore } from '@/store/pointer/pointer.store';
+
 export const useComposerClick = () => {
+  const { setLastEvent } = usePointerStore();
+
   const isClickInsideComposer = (event: MouseEvent) =>
     event
       .composedPath()
@@ -6,7 +10,14 @@ export const useComposerClick = () => {
         (el as HTMLElement)?.classList?.contains('lb-composer-editor-actions'),
       );
 
+  const savePointerEvent = (event: PointerEvent) => {
+    (event as any)._savedComposedPath = event?.composedPath();
+
+    setLastEvent(event);
+  };
+
   return {
     isClickInsideComposer,
+    savePointerEvent,
   };
 };
