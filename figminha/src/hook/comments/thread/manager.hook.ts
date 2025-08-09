fd012cf859c7c66ed5697b54280cfdf4af22a450
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-08-09 17:15:49 -0300

    [ :thread: ] | update: hook - thread > manager [subscribers] (app::hook)

--- figminha/src/hook/comments/thread/manager.hook.ts
+++ figminha/src/hook/comments/thread/manager.hook.ts
@@ -1,36 +1,12 @@
-import { useCallback, useEffect } from 'react';
-import { useCommentCreationState } from '../state/creation.hook';
-import { useComposerHandler } from './composer/handler.hook';
+import { useCommentCreationSubscriber } from './events/comment/new.hook';
+import { usePlacementCancelSubscriber } from './events/composer/placement/cancel.hook';
+import { useComposerAllowmentSubscriber } from './events/composer/use.hook';
+import { usePointerMoveSubscriber } from './events/pointer/move.hook';
 
 export const useThreadManager = () => {
-  const {
-    state: commentState,
-    isCompleted,
-    isPlaced,
-  } = useCommentCreationState();
+  useCommentCreationSubscriber();
+  usePointerMoveSubscriber();
 
-  const { closeComposer, placeComposer } = useComposerHandler();
-
-  const handleNewCommentClick = useCallback(
-    (event: MouseEvent) => {
-      event.preventDefault();
-      if (isPlaced) return closeComposer(event);
-
-      placeComposer(event);
-    },
-    [isPlaced, closeComposer, placeComposer],
-  );
-
-  useEffect(() => {
-    if (isCompleted) return;
-
-    document.documentElement.addEventListener('click', handleNewCommentClick);
-
-    return () => {
-      document.documentElement.removeEventListener(
-        'click',
-        handleNewCommentClick,
-      );
-    };
-  }, [commentState, handleNewCommentClick, isCompleted]);
+  usePlacementCancelSubscriber();
+  useComposerAllowmentSubscriber();
 };
