f8601643a8aa8102f12cf2f3a79a24b8f06fd77f
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-08-09 16:25:39 -0300

    [ :label: ] | update: type - store > composer (@type::comments)

--- figminha/src/@types/store/composer/composer.type.ts
+++ figminha/src/@types/store/composer/composer.type.ts
@@ -2,6 +2,8 @@ import { IPosition } from '@/@types/position.type';
 
 export interface IComposerStore {
   coordinates: IPosition;
+  allowComposer: boolean;
 
   setCoordinates: (coordinates: IPosition) => void;
+  setAllowComposer: (allowComposer: boolean) => void;
 }
