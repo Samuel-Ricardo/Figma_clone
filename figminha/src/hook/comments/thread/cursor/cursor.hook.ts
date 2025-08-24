21d53893df5a11ee1815bf6f7c56dc6a080720f5
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-08-24 16:24:47 -0300

    [ :speech_balloon: ] | create: hook - thread > cursor [get::canvas] (app::hook::comments)

--- figminha/src/hook/comments/thread/cursor/cursor.hook.ts
+++ figminha/src/hook/comments/thread/cursor/cursor.hook.ts
@@ -3,6 +3,9 @@ import { useThreadCursorStore } from '@/store/cursor/thread/cursor.store';
 export const useNewThreadCursor = () => {
   const { setCoordinates, resetCursor } = useThreadCursorStore();
 
+  const getCanvasRect = () =>
+    document.getElementById('canvas')?.getBoundingClientRect();
+
   const updatePosition = (e: MouseEvent) => {
     if (isCursorOutsideCanvas(e, getCanvasRect())) return resetCursor();
 
