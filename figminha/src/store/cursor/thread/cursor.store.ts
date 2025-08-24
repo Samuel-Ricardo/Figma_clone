ce9024dbb58da94368eb3b6b268154fb43b3eeda
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-08-24 15:51:47 -0300

    [ :link: ] | create: store - comment > thread [cursor::reset] (app::store)

--- figminha/src/store/cursor/thread/cursor.store.ts
+++ figminha/src/store/cursor/thread/cursor.store.ts
@@ -6,4 +6,8 @@ const DEFAULT_CURSOR_POSITION = -10000;
 export const useThreadCursorStore = create<INewThreadCursorStore>(set => ({
   coordinates: { x: DEFAULT_CURSOR_POSITION, y: DEFAULT_CURSOR_POSITION },
   setCoordinates: coordinates => set({ coordinates }),
+  resetCursor: () =>
+    set({
+      coordinates: { x: DEFAULT_CURSOR_POSITION, y: DEFAULT_CURSOR_POSITION },
+    }),
 }));
