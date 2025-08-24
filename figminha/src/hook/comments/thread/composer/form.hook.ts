bf69e86a5f2bfd38cdbfefe2a4278b8ef2916fbe
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-08-24 16:42:33 -0300

    [ :speech_balloon: ] | create: hook - thread > composer [form] (app::hook::comments)

--- figminha/src/hook/comments/thread/composer/form.hook.ts
+++ figminha/src/hook/comments/thread/composer/form.hook.ts
@@ -1,1 +1,19 @@
-export const useComposerForm = () => {};
+import { IPosition } from '@/@types/position.type';
+import { useComposerStore } from '@/store/composer/composer.store';
+import { usePointerStore } from '@/store/pointer/pointer.store';
+import { ComposerSubmitComment } from '@liveblocks/react-comments';
+import { FormEvent, useCallback } from 'react';
+import { useCommentThread } from '../thread.hook';
+import { useComposerHandler } from './handler.hook';
+
+export const useComposerForm = () => {
+  const { coordinates } = useComposerStore();
+  const { lastPointerEvent } = usePointerStore();
+  const { newThread } = useCommentThread();
+  const { resetComposerState } = useComposerHandler();
+
+  const stopFormSubmission = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+};
