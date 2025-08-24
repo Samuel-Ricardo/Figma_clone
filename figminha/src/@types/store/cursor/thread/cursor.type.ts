ab8a5c74b6a2e6aafdbc27ac54839722cf9093e7
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-08-24 15:58:08 -0300

    [ :label: ] | create: type - store > thread [cursor::reset] (@type::comments)

--- figminha/src/@types/store/cursor/thread/cursor.type.ts
+++ figminha/src/@types/store/cursor/thread/cursor.type.ts
@@ -3,4 +3,5 @@ import { IPosition } from '@/@types/position.type';
 export interface INewThreadCursorStore {
   coordinates: IPosition;
   setCoordinates: (coordinates: IPosition) => void;
+  resetCursor: () => void;
 }
