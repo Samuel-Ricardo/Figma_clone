c53047c3dc99e6fd338acf71842840b9126e9c16
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-08-24 15:59:05 -0300

    [ :label: ] | update: type - store > canvas [shape] (@type)

--- figminha/src/@types/store/canvas/shape/shape.type.ts
+++ figminha/src/@types/store/canvas/shape/shape.type.ts
@@ -1,14 +1,17 @@
 import { ShapeType } from '@/@types/canvas/shape/type.type';
 import { Object } from 'fabric';
+import { ActiveElement } from '../element/active.type';
 
 export interface IShapeStore {
   shapeRef: React.MutableRefObject<Object | null> | null;
-  selectedShapeRef: ShapeType | null;
+  selectedShapeRef: ShapeType | string | ActiveElement[] | null;
 
   setShapeRef: (shape: React.MutableRefObject<Object | null> | null) => void;
   clearShapeRef: () => void;
 
-  setSelectedShapeRef: (shape: string | null) => void;
+  setSelectedShapeRef: (
+    shape: string | ShapeType | ActiveElement[] | null,
+  ) => void;
   clearSelectedShapeRef: () => void;
 
   clearAllShapeRefs: () => void;
